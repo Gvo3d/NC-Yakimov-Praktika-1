29a8242ed13e9fe83c225cf1b65716f9ce424f03
Gvo3d <denis.yakimov89@gmail.com>
2017-03-15 20:06:34 +0200

    Second homework, bubbles sort and sout args in command line

--- src/org/yakimovdenis/Main.java
+++ src/org/yakimovdenis/Main.java
@@ -3,6 +3,35 @@ package org.yakimovdenis;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length > 0) {
+            print(args);
+        } else {
+            sort("This is test text for second task".split(" "));
+        }
+    }
+
+    public static void print(String[] args) {
+        if (args.length > 0) {
+            for (int argumentNumber = 0; argumentNumber < args.length; argumentNumber++)
+                System.out.println(args[argumentNumber]);
+        }
+    }
+
+    //Bubbles sort algoritm
+    public static void sort(String[] args) {
+        if (null != args && args.length > 0) {
+            String bubble;
+            for (int j=0; j<args.length-1; j++){
+                boolean temp = false;
+                for (int i=0; i<args.length-j-1; i++){
+                    if (args[i].compareTo(args[i+1])>0) { bubble=args[i];
+                    args[i] = args[i+1];
+                    args[i+1]=bubble;
+                    } else  temp=true;
+                }
+                if (temp==false) break;
+            }
+            print(args);
+        }
     }
 }
